7fd50fd98728fa4792b06d4d695ff1e6ab4ea826
NEYROSICK <dhatak24@gmail.com>
2023-06-06 22:36:53 +0300

    General Upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+
+galleryListEl.innerHTML = galleryMarkup;
+
+const handleClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName != "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", (keyEvent) => {
+    if (keyEvent.code === "Escape") {
+      instance.close();
+    }
+  });
+};
+
+galleryListEl.addEventListener("click", handleClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join("");
+
+galleryListEl.innerHTML = galleryMarkup;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
